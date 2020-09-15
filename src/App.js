109e75c363410ea79590d7192753be098980790f
evorio <support@evorio.co.uk>
2020-09-15 22:10:48 +0100

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const element = <h1>Hello World</h1>;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {element}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Components/counter.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
+
+  render() {
+    return (
+      <div className="">
+        {/* <img src={this.state.imageUrl} /> */}
+        <br />
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-small">Increment</button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
